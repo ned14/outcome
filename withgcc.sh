64ccde1662a32d0032d6a26c034139c091a664c2
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spam@nowhere>
2014-07-18 19:59:56 +0100

    Now outputs both single and multi threaded tests.

--- withgcc.sh
+++ withgcc.sh
@@ -1,2 +1,4 @@
-g++-4.8 -std=c++0x -O3 -DNDEBUG -g -gdwarf-2 -o unittests unittests.cpp -lrt
-g++-4.8 -std=c++0x -O3 -DNDEBUG -g -fopenmp -fgnu-tm -DBOOST_HAVE_TRANSACTIONAL_MEMORY_COMPILER -o unittests_stm unittests.cpp -lrt
+g++-4.8 -std=c++0x -O3 -DNDEBUG -g -gdwarf-2 -o unittests_1 unittests.cpp -lrt
+g++-4.8 -std=c++0x -O3 -DNDEBUG -g -fopenmp -gdwarf-2 -o unittests_N unittests.cpp -lrt
+g++-4.8 -std=c++0x -O3 -DNDEBUG -g -fgnu-tm -DBOOST_HAVE_TRANSACTIONAL_MEMORY_COMPILER -o unittests_stm_1 unittests.cpp -lrt
+g++-4.8 -std=c++0x -O3 -DNDEBUG -g -fopenmp -fgnu-tm -DBOOST_HAVE_TRANSACTIONAL_MEMORY_COMPILER -o unittests_stm_N unittests.cpp -lrt
