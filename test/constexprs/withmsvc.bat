61ab65e89f7d9b20b5c36672d816fe897ca84872
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nowhere.com>
2015-06-04 14:20:05 +0100

    Fix bad xml

--- test/constexprs/withmsvc.bat
+++ test/constexprs/withmsvc.bat
@@ -8,6 +8,7 @@ for %%f in (*.cpp) do (
   cl /EHsc /c /O2 /GS- /GR /Gy /Zc:inline /DBOOST_SPINLOCK_STANDALONE=1 %%f /I..\..\include\boost\spinlock\bindlib\include
   dumpbin /disasm !FILE!.obj > !FILE!.msvc.S
   del !FILE!.obj
+  set LINE=
   for /f %%i in ('count_opcodes.py !FILE!.msvc.S') do set LINE=%%i
   if "!MSVCLINE!" == "" (
     set MSVCLINE=!LINE!
@@ -16,7 +17,7 @@ for %%f in (*.cpp) do (
   )
   echo   ^<testcase name="!FILE!.msvc"^> >> results.xml
   if !LINE! GTR 5 (
-    echo     ^<failure message="Opcodes generated !LINE! exceeds 5"^> >> results.xml
+    echo     ^<failure message="Opcodes generated !LINE! exceeds 5"/^> >> results.xml
   )
   echo     ^<system-out^> >> results.xml
   echo ^<^^![CDATA[ >> results.xml
