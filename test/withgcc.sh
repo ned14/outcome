6a653f3d1f628bd7c42ca879524529693aa58c7e
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nowhere.com>
2015-06-13 18:03:43 +0100

    Broke coverage.

--- test/withgcc.sh
+++ test/withgcc.sh
@@ -9,7 +9,7 @@ rm -rf unittests_* *.gcda *.gcno
 INCLUDE="-I../include/boost/spinlock/bindlib/include -I../include/boost/spinlock/expected/include"
 if [ -n "$BUILD_EXTRA" ]; then
   echo Building unittests_coverage ...
-  g++ -std=c++14 -pthread -O1 -DNDEBUG -DRUNNING_ON_VALGRIND=1 -g -gdwarf-2 -o unittests_coverage unittests.cpp -lrt -fprofile-arcs -ftest-coverage -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-elide-constructors -fno-inline $INCLUDE
+  g++ -std=c++0x -pthread -O1 -DNDEBUG -DRUNNING_ON_VALGRIND=1 -g -gdwarf-2 -o unittests_coverage unittests.cpp -lrt -fprofile-arcs -ftest-coverage -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-elide-constructors -fno-inline $INCLUDE
 fi
 echo C++ 11 check
 clang++-3.7 -Wall -Wextra -std=c++11 -pthread -O3 -DNDEBUG -g -gdwarf-2 -o unittests_1 unittests.cpp -lrt $INCLUDE -Wno-unknown-pragmas -Wno-unused-function || true
