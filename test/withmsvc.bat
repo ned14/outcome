853b5fb1286a5b3aaed9aedf3927ad4b00782f20
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nowhere.com>
2015-06-16 17:51:08 +0100

    Have withmsvc generate single and multithreaded test executables

--- test/withmsvc.bat
+++ test/withmsvc.bat
@@ -1,1 +1,7 @@
+del /q unittests_*.exe
 cl /EHsc /O2 /DNDEBUG /DBOOST_SPINLOCK_STANDALONE=1 unittests.cpp -I..\include\boost\spinlock\bindlib\include
+
+rename unittests.exe unittests_1.exe
+cl /openmp /EHsc /O2 /DNDEBUG /DBOOST_SPINLOCK_STANDALONE=1 unittests.cpp -I..\include\boost\spinlock\bindlib\include
+
+rename unittests.exe unittests_N.exe
