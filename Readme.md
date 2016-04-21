a9202c138dc4e74fee59177ca168a12212f32743
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2016-04-21 18:32:05 +0100

    Try again with Travis, and update Readme to point at Travis + Appveyor

--- Readme.md
+++ Readme.md
@@ -1,9 +1,9 @@
 <center>
 master branch unit test status:
 
-Linux: [![Build Status](https://ci.nedprod.com/job/Boost.Monad%20Test%20Linux%20GCC%205/badge/icon)](https://ci.nedprod.com/job/Boost.Monad%20Test%20Linux%20GCC%205/) Windows: [![Build Status](https://ci.nedprod.com/job/Boost.Monad%20Test%20Win8%20VS2014/badge/icon)](https://ci.nedprod.com/job/Boost.Monad%20Test%20Win8%20VS2014/) Coverage: [![Coverage Status](https://coveralls.io/repos/ned14/boost.outcome/badge.svg?branch=master)](https://coveralls.io/r/ned14/boost.outcome?branch=master) Docs: [![Build Status](https://ci.nedprod.com/job/Boost.Monad%20Documentation/badge/icon)](https://ci.nedprod.com/job/Boost.Monad%20Documentation)
+Linux: [![Build Status](https://travis-ci.org/ned14/boost.outcome.svg?branch=master)](https://travis-ci.org/ned14/boost.outcome) Windows: [![Build Status](https://ci.nedprod.com/job/Boost.Monad%20Test%20Win8%20VS2014/badge/icon)](https://ci.nedprod.com/job/Boost.Monad%20Test%20Win8%20VS2014/) Coverage: [![Build status](https://ci.appveyor.com/api/projects/status/roe4dacos4gnlu66/branch/master?svg=true)](https://ci.appveyor.com/project/ned14/boost-outcome/branch/master)
 
-Documentation: https://ned14.github.io/boost.outcome/ (PDF: https://ned14.github.io/boost.outcome/refman.pdf)
+Documentation: https://ned14.github.io/boost.outcome/
 
 Outcome/Result/Option:
  - [ ] Add constexpr compile unit tests for when <T> is a LiteralType.
--- test/withgcc.sh
+++ test/withgcc.sh
@@ -9,7 +9,7 @@ rm -rf unittests_* *.gcda *.gcno
 INCLUDE=""
 if [ -n "$BUILD_EXTRA" ]; then
   echo Building unittests_coverage ...
-  g++-4.8 -std=c++0x -pthread -O1 -DNDEBUG -DRUNNING_ON_VALGRIND=1 -g -gdwarf-2 -o unittests_coverage unittests.cpp -lrt -fprofile-arcs -ftest-coverage -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-elide-constructors -fno-inline $INCLUDE
+  g++-4.9 -std=c++0x -pthread -O1 -DNDEBUG -DRUNNING_ON_VALGRIND=1 -g -gdwarf-2 -o unittests_coverage unittests.cpp -lrt -fprofile-arcs -ftest-coverage -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-elide-constructors -fno-inline $INCLUDE
 fi
 echo C++ 14 check
 time clang++-3.7 -Wall -Wextra -std=c++14 -pthread -O3 -DNDEBUG -DBOOST_OUTCOME_ENABLE_OPERATORS=1 -g -gdwarf-2 -o unittests_1 unittests.cpp -lrt $INCLUDE -Wno-unknown-pragmas -Wno-unused-function || true
@@ -29,7 +29,7 @@ if [ -n "$BUILD_EXTRA" ]; then
 #  echo Building unittests_stm_N ...
 #  $CXX -std=c++0x -O3 -DNDEBUG -g -fopenmp -fgnu-tm -DBOOST_HAVE_TRANSACTIONAL_MEMORY_COMPILER -o unittests_stm_N unittests.cpp -lrt $INCLUDE
   echo Building unittests_sanitise ...
-  #g++-4.8 -std=c++0x -fsanitize=thread -fPIC -pie -O0 -DNDEBUG -g -fopenmp -gdwarf-2 -o unittests_sanitise unittests.cpp -lrt -ltsan -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-elide-constructors -I$BOOSTCONFIG
+  #g++-4.9 -std=c++0x -fsanitize=thread -fPIC -pie -O0 -DNDEBUG -g -fopenmp -gdwarf-2 -o unittests_sanitise unittests.cpp -lrt -ltsan -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-elide-constructors -I$BOOSTCONFIG
   #clang++-3.4 -std=c++0x -fsanitize=thread -O2 -DNDEBUG -g -o unittests_sanitise unittests.cpp -lrt -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-elide-constructors -fno-inline $INCLUDE
   $CXX -std=c++1y -fsanitize=undefined -fsanitize=thread -fPIC -pie -O1 -DNDEBUG -DRUNNING_ON_VALGRIND=1 -D__SANITIZE_ADDRESS__=1 -g -fopenmp -gdwarf-2 -o unittests_sanitise unittests.cpp -lrt -ltsan -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-elide-constructors -fno-inline $INCLUDE
 fi
