4f4fe42313a34f6f6ba121aee0c1beba087a7e97
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2016-06-21 14:35:47 +0100

    travis

--- test/withgcc.sh
+++ test/withgcc.sh
@@ -12,11 +12,11 @@ if [ -n "$BUILD_EXTRA" ]; then
   g++-4.9 -std=c++0x -pthread -O1 -DNDEBUG -DRUNNING_ON_VALGRIND=1 -g -gdwarf-2 -o unittests_coverage unittests.cpp -lrt -fprofile-arcs -ftest-coverage -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-elide-constructors -fno-inline $INCLUDE
 fi
 echo C++ 14 check
-time clang++-3.7 -Wall -Wextra -std=c++14 -pthread -O3 -DNDEBUG -DBOOST_OUTCOME_ENABLE_OPERATORS=1 -g -gdwarf-2 -o unittests_1 unittests.cpp -lrt $INCLUDE -Wno-unknown-pragmas -Wno-unused-function || true
+time clang++-3.8 -Wall -Wextra -std=c++14 -pthread -O3 -DNDEBUG -DBOOST_OUTCOME_ENABLE_OPERATORS=1 -g -gdwarf-2 -o unittests_1 unittests.cpp -lrt $INCLUDE -Wno-unknown-pragmas -Wno-unused-function || true
 echo C++ 11 check
-time clang++-3.7 -Wall -Wextra -std=c++11 -pthread -O3 -DNDEBUG -DBOOST_OUTCOME_ENABLE_OPERATORS=1 -g -gdwarf-2 -o unittests_1 unittests.cpp -lrt $INCLUDE -Wno-unknown-pragmas -Wno-unused-function || true
+time clang++-3.8 -Wall -Wextra -std=c++11 -pthread -O3 -DNDEBUG -DBOOST_OUTCOME_ENABLE_OPERATORS=1 -g -gdwarf-2 -o unittests_1 unittests.cpp -lrt $INCLUDE -Wno-unknown-pragmas -Wno-unused-function || true
 echo No operators check
-time clang++-3.7 -Wall -Wextra -std=c++11 -pthread -O3 -DNDEBUG -g -gdwarf-2 -o unittests_nooperators unittests.cpp -lrt $INCLUDE -Wno-unknown-pragmas -Wno-unused-function || true
+time clang++-3.8 -Wall -Wextra -std=c++11 -pthread -O3 -DNDEBUG -g -gdwarf-2 -o unittests_nooperators unittests.cpp -lrt $INCLUDE -Wno-unknown-pragmas -Wno-unused-function || true
 echo Building unittests_valgrind ...
 time $CXX -std=c++1z -pthread -O1 -DNDEBUG -DBOOST_OUTCOME_ENABLE_OPERATORS=1 -g -gdwarf-2 -o unittests_valgrind unittests.cpp -lrt -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-elide-constructors -fno-inline -DBOOST_SPINLOCK_ENABLE_VALGRIND $INCLUDE
 echo Building unittests_1 ...
