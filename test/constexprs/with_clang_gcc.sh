e592db760ba0c172fc00d0669bca7dc2b262dca7
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2016-06-21 14:48:56 +0100

    travis

--- test/constexprs/with_clang_gcc.sh
+++ test/constexprs/with_clang_gcc.sh
@@ -26,7 +26,7 @@ do
   
   if [ 1 -eq 1 ]; then
     echo "Compiling ${FILE} with gcc ..."
-    g++-5 -c -o $FILE.o -O3 -std=c++14 -fno-keep-inline-functions -DBOOST_OUTCOME_ENABLE_OPERATORS=1 -DSPINLOCK_STANDALONE=1 -DNDEBUG $f
+    g++-6 -c -o $FILE.o -O3 -std=c++14 -fno-keep-inline-functions -DBOOST_OUTCOME_ENABLE_OPERATORS=1 -DSPINLOCK_STANDALONE=1 -DNDEBUG $f
     objdump -d -S $FILE.o > $FILE.gcc.S
 
     echo "Compiling ${FILE} with clang ..."
