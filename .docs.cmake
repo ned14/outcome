0bea6b3b2fd8e8400c844fd9e5b12e847b3ad50d
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2016-08-07 18:35:46 +0100

    Theoretically Travis should now push latest docs every commit to github. I bet it doesn't actually work ...

---
+++ .docs.cmake
@@ -0,0 +1,35 @@
+# CTest script for a CI to submit to CDash a documentation generation run
+cmake_minimum_required(VERSION 3.0 FATAL_ERROR)
+# Bring in the Boost lite cmake tooling
+list(FIND CMAKE_MODULE_PATH "boost-lite" boost_lite_idx)
+if(${boost_lite_idx} EQUAL -1)
+  if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/../boost-lite/cmake")
+    set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_CURRENT_SOURCE_DIR}/../boost-lite/cmake")
+    set(CTEST_BOOSTLITE_SCRIPTS "${CMAKE_CURRENT_SOURCE_DIR}/../boost-lite/scripts")
+  elseif(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/include/boost/outcome/boost-lite/cmake")
+    set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_CURRENT_SOURCE_DIR}/include/boost/outcome/boost-lite/cmake")
+    set(CTEST_BOOSTLITE_SCRIPTS "${CMAKE_CURRENT_SOURCE_DIR}/include/boost/outcome/boost-lite/scripts")
+  else()
+    message(FATAL_ERROR "FATAL: A copy of boost-lite cannot be found. Try running 'git submodule update --init --recursive'")
+  endif()
+endif()
+include(BoostLiteUtils)
+
+
+CONFIGURE_CTEST_SCRIPT_FOR_CDASH("outcome" "cmake_ci")
+ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+include(FindGit)
+checked_execute_process("git reset"
+  COMMAND "${GIT_EXECUTABLE}" reset --hard ae7119571a3c81cb9a683a21f2759df1d742e998
+  WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/doc/html"
+)
+
+ctest_start("Documentation")
+ctest_configure()
+ctest_build(TARGET outcome_docs)
+checked_execute_process("git commit"
+  COMMAND "${GIT_EXECUTABLE}" commit -a -m "upd"
+  COMMAND "${GIT_EXECUTABLE}" push -f https://jenkins-nedprod:$ENV{JENKINS_NEDPROD_PASSWORD}@github.com/ned14/boost.outcome
+  WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/doc/html"
+)
+#ctest_submit()
--- release_notes.md
+++ release_notes.md
@@ -1,9 +1,15 @@
 This is the proposed Boost.Outcome library, a Boost C++ 14 library providing
 \ref monad, a factory and family of policy driven lightweight monads with
-the specialisations of `outcome<T>`, `result<T>` and `option<T>`. Its github
-source code repository lives at https://github.com/ned14/boost.outcome.
+the specialisations of `outcome<T>`, `result<T>` and `option<T>`.
+
+Its github source code repository lives at https://github.com/ned14/boost.outcome.
+
+Linux: [![Build Status](https://travis-ci.org/ned14/boost.outcome.svg?branch=master)](https://travis-ci.org/ned14/boost.outcome) Windows: [![Build status](https://ci.appveyor.com/api/projects/status/roe4dacos4gnlu66/branch/master?svg=true)](https://ci.appveyor.com/project/ned14/boost-outcome/branch/master) Coverage: [![Coverage Status](https://coveralls.io/repos/ned14/boost.outcome/badge.svg?branch=master)](https://coveralls.io/r/ned14/boost.outcome?branch=master) CTest dashboard: http://my.cdash.org/index.php?project=Boost.Outcome
 
 You need these compilers or better:
 - GCC 5.0
 - clang 3.7
 - VS2015 Update 2
+- clang 3.7 with Microsoft Codegen ("winclang")
+
+Its sole mandatory dependency is on https://github.com/ned14/boost-lite, a minimal emulation of Boost for C++ 11.
