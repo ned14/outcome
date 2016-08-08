6dc078c891ae98a889f12477faa3729bf4949688
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2016-08-08 15:12:11 +0100

    travis

--- .docs.cmake
+++ .docs.cmake
@@ -19,18 +19,18 @@ include(BoostLiteUtils)
 CONFIGURE_CTEST_SCRIPT_FOR_CDASH("outcome" "cmake_ci")
 ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
 include(FindGit)
-checked_execute_process("git reset"
-  COMMAND "${GIT_EXECUTABLE}" checkout gh-pages
+#checked_execute_process("git reset"
+#  COMMAND "${GIT_EXECUTABLE}" checkout gh-pages
 #  COMMAND "${GIT_EXECUTABLE}" reset --hard ae7119571a3c81cb9a683a21f2759df1d742e998
-  WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/doc/html"
-)
+#  WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/doc/html"
+#)
 
 ctest_start("Documentation")
 ctest_configure()
 ctest_build(TARGET outcome_docs)
-checked_execute_process("git commit"
-  COMMAND "${GIT_EXECUTABLE}" commit -a -m "upd"
+#checked_execute_process("git commit"
+#  COMMAND "${GIT_EXECUTABLE}" commit -a -m "upd"
 #  COMMAND "${GIT_EXECUTABLE}" push -f origin gh-pages
-  WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/doc/html"
-)
+#  WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/doc/html"
+#)
 ctest_submit()
