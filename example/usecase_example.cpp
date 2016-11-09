9257a1bf8ebf65d7c308ef822540cfc43c1cbab6
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2016-11-09 09:18:12 +0000

    tutorial

---
+++ example/usecase_example.cpp
@@ -0,0 +1,199 @@
+/* A small standalone fragment of AFIO
+*/
+
+#ifdef _WIN32
+
+#include "../include/boost/outcome.hpp"
+
+#include "../boost-lite/include/bitfield.hpp"
+#include <filesystem>
+
+namespace minimal_afio
+{
+  using namespace BOOST_OUTCOME_V1_NAMESPACE;
+  struct native_handle_type
+  {
+    HANDLE h;
+    native_handle_type()
+        : h(nullptr)
+    {
+    }
+  };
+  struct file_handle
+  {
+    using path_type = std::experimental::filesystem::path;
+    enum class mode : unsigned char
+    {
+      unchanged = 0,
+      none = 2,        //!< No ability to read or write anything, but can synchronise (SYNCHRONIZE or 0)
+      attr_read = 4,   //!< Ability to read attributes (FILE_READ_ATTRIBUTES|SYNCHRONIZE or O_RDONLY)
+      attr_write = 5,  //!< Ability to read and write attributes (FILE_READ_ATTRIBUTES|FILE_WRITE_ATTRIBUTES|SYNCHRONIZE or O_RDONLY)
+      read = 6,        //!< Ability to read (READ_CONTROL|FILE_READ_DATA|FILE_READ_ATTRIBUTES|FILE_READ_EA|SYNCHRONISE or O_RDONLY)
+      write = 7,       //!< Ability to read and write (READ_CONTROL|FILE_READ_DATA|FILE_READ_ATTRIBUTES|FILE_READ_EA|FILE_WRITE_DATA|FILE_WRITE_ATTRIBUTES|FILE_WRITE_EA|FILE_APPEND_DATA|SYNCHRONISE or O_RDWR)
+      append = 9       //!< All mainstream OSs and CIFS guarantee this is atomic with respect to all other appenders (FILE_APPEND_DATA|SYNCHRONISE or O_APPEND)
+    };
+    enum class creation : unsigned char
+    {
+      open_existing = 0,
+      only_if_not_exist,
+      if_needed,
+      truncate  //!< Atomically truncate on open, leaving creation date unmodified.
+    };
+    enum class caching : unsigned char  // bit 0 set means safety fsyncs enabled
+    {
+      unchanged = 0,
+      none = 1,                //!< No caching whatsoever, all reads and writes come from storage (i.e. <tt>O_DIRECT|O_SYNC</tt>). Align all i/o to 4Kb boundaries for this to work. <tt>flag_disable_safety_fsyncs</tt> can be used here.
+      only_metadata = 2,       //!< Cache reads and writes of metadata but avoid caching data (<tt>O_DIRECT</tt>), thus i/o here does not affect other cached data for other handles. Align all i/o to 4Kb boundaries for this to work.
+      reads = 3,               //!< Cache reads only. Writes of data and metadata do not complete until reaching storage (<tt>O_SYNC</tt>). <tt>flag_disable_safety_fsyncs</tt> can be used here.
+      reads_and_metadata = 5,  //!< Cache reads and writes of metadata, but writes of data do not complete until reaching storage (<tt>O_DSYNC</tt>). <tt>flag_disable_safety_fsyncs</tt> can be used here.
+      all = 4,                 //!< Cache reads and writes of data and metadata so they complete immediately, sending writes to storage at some point when the kernel decides (this is the default file system caching on a system).
+      safety_fsyncs = 7,       //!< Cache reads and writes of data and metadata so they complete immediately, but issue safety fsyncs at certain points. See documentation for <tt>flag_disable_safety_fsyncs</tt>.
+      temporary = 6            //!< Cache reads and writes of data and metadata so they complete immediately, only sending any updates to storage on last handle close in the system or if memory becomes tight as this file is expected to be temporary (Windows only).
+                               // NOTE: IF UPDATING THIS UPDATE THE std::ostream PRINTER BELOW!!!
+    };
+    BOOSTLITE_BITFIELD_BEGIN(flag)
+    {
+      none = 0,  //!< No flags
+      unlink_on_close = 1 << 0, disable_safety_fsyncs = 1 << 2, disable_safety_unlinks = 1 << 3,
+
+      win_disable_unlink_emulation = 1 << 24,  //!< See the documentation for `unlink_on_close`
+
+      overlapped = 1 << 28,         //!< On Windows, create any new handles with OVERLAPPED semantics
+      byte_lock_insanity = 1 << 29  //!< Using insane POSIX byte range locks
+    }
+    BOOSTLITE_BITFIELD_END(flag)
+    file_handle(path_type path, caching, flag)
+        : _path(std::move(path))
+
+    {
+    }
+    native_handle_type _v;
+    path_type _path;
+  };
+  using handle = file_handle;
+  static inline result<ACCESS_MASK> access_mask_from_handle_mode(handle::mode _mode)
+  {
+    ACCESS_MASK access = SYNCHRONIZE;
+    switch(_mode)
+    {
+    case handle::mode::unchanged:
+      return make_errored_result<ACCESS_MASK>(EINVAL);
+    case handle::mode::none:
+      break;
+    case handle::mode::attr_read:
+      access |= FILE_READ_ATTRIBUTES;
+      break;
+    case handle::mode::attr_write:
+      access |= FILE_READ_ATTRIBUTES | FILE_WRITE_ATTRIBUTES;
+      break;
+    case handle::mode::read:
+      access |= GENERIC_READ;
+      break;
+    case handle::mode::write:
+      access |= GENERIC_WRITE | GENERIC_READ;
+      break;
+    case handle::mode::append:
+      access |= FILE_APPEND_DATA;
+      break;
+    }
+    return access;
+  }
+  static inline result<DWORD> attributes_from_handle_caching_and_flags(handle::caching _caching, handle::flag flags)
+  {
+    DWORD attribs = 0;
+    if(flags & handle::flag::overlapped)
+    {
+      attribs |= FILE_FLAG_OVERLAPPED;
+    }
+    switch(_caching)
+    {
+    case handle::caching::unchanged:
+      return make_errored_result<DWORD>(EINVAL);
+    case handle::caching::none:
+      attribs |= FILE_FLAG_NO_BUFFERING | FILE_FLAG_WRITE_THROUGH;
+      break;
+    case handle::caching::only_metadata:
+      attribs |= FILE_FLAG_NO_BUFFERING;
+      break;
+    case handle::caching::reads:
+    case handle::caching::reads_and_metadata:
+      attribs |= FILE_FLAG_WRITE_THROUGH;
+      break;
+    case handle::caching::all:
+    case handle::caching::safety_fsyncs:
+      break;
+    case handle::caching::temporary:
+      attribs |= FILE_ATTRIBUTE_TEMPORARY;
+      break;
+    }
+    if(flags & handle::flag::unlink_on_close)
+      attribs |= FILE_FLAG_DELETE_ON_CLOSE;
+    return attribs;
+  }
+}
+
+namespace file_create
+{
+  //! [file_create_example]
+  using namespace minimal_afio;
+  using boost_lite::ringbuffer_log::last190;
+  extern BOOST_SYMBOL_EXPORT result<file_handle> file_create(file_handle::path_type _path, file_handle::mode _mode = file_handle::mode::read, file_handle::creation _creation = file_handle::creation::open_existing, file_handle::caching _caching = file_handle::caching::all,
+                                                             file_handle::flag flags = file_handle::flag::none) noexcept
+  {
+    // noexcept functions should always wrap their innards in a try...catch,
+    // if you don't ever call a potentially throwing piece of code the compiler
+    // will completely elide this.
+    try
+    {
+      result<file_handle> ret(file_handle(std::move(_path), _caching, flags));
+      native_handle_type &nativeh = ret.value()._v;
+      // access_mask_from_handle_mode() returns a result<ACCESS_MASK>,
+      // if it is signalled then propagate it immediately by returning it,
+      // else unwrap the return value into the variable access
+      BOOST_OUTCOME_TRY(access, access_mask_from_handle_mode(_mode));
+      DWORD creation = OPEN_EXISTING;
+      switch(_creation)
+      {
+      case file_handle::creation::open_existing:
+        break;
+      case file_handle::creation::only_if_not_exist:
+        creation = CREATE_NEW;
+        break;
+      case file_handle::creation::if_needed:
+        creation = OPEN_ALWAYS;
+        break;
+      case file_handle::creation::truncate:
+        creation = TRUNCATE_EXISTING;
+        break;
+      }
+      // attributes_from_handle_caching_and_flags() returns a result<DWORD>,
+      // so if it errors the propagate immediately, else unpack DWORD into
+      // the variable attribs
+      BOOST_OUTCOME_TRY(attribs, attributes_from_handle_caching_and_flags(_caching, flags));
+      if(INVALID_HANDLE_VALUE == (nativeh.h = CreateFile(ret.value()._path.c_str(), access, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL, creation, attribs, NULL)))
+      {
+        DWORD errcode = GetLastError();
+        // make_errored_result() understands DWORDs on Windows. It also understands
+        // int for automatic errno conversion.
+        return make_errored_result<file_handle>(errcode, last190(ret.value()._path.u8string()));
+      }
+      if(flags & file_handle::flag::unlink_on_close)
+      {
+        // Hide this item
+        SetFileAttributes(ret.value()._path.c_str(), FILE_ATTRIBUTE_HIDDEN);
+      }
+      return ret;
+    }
+    // Catch all the STL exceptions, converting them to an equivalent result<>
+    // containing a system_error with the appropriate error_code.
+    BOOST_OUTCOME_CATCH_EXCEPTION_TO_RESULT
+  }
+  //! [file_create_example]
+}
+
+#endif
+
+int main(void)
+{
+  return 0;
+}
--- release_notes.md
+++ release_notes.md
@@ -899,8 +899,8 @@ template <class T> constexpr inline option<void> as_void(const option<T> &v)
 
 // Equivalent to try! in Rust and Swift
 #define BOOST_OUTCOME_TRY(variable_to_initialise, monad)
-// Boilerplate catching all the STL exceptions, returning a result<type>
-#define BOOST_OUTCOME_CATCH_EXCEPTION_TO_RESULT(type)
+// Boilerplate catching all the STL exceptions, returning an equivalent result<void>
+#define BOOST_OUTCOME_CATCH_EXCEPTION_TO_RESULT
 
 BOOST_OUTCOME_V1_NAMESPACE_END
 \endcode
@@ -918,4 +918,102 @@ macro `BOOST_OUTCOME_DISABLE_DEFAULT_SINGLE_BYTE_VALUE_STORAGE` is defined.
 internal non-accessible type usefully named to indicate you are trying to use
 something not possible which will generate a very obvious descriptive compiler error.
 
+
+\section examples Examples of usage
+
+The above synopsis of Outcome looks lengthy and complex, but almost all of it is
+convenience overloads of one form or other. In usage Outcome is generally "stupid easy"
+to program with. Here is a real world use case distilled from AFIO v2:
+
+\snippet usecase_example.cpp file_create_example
+
+
 To be continued ...
+
+
+\section functional Functional programming extensions (optional)
+
+\note All code in this section can be enabled by defining `BOOST_OUTCOME_ENABLE_OPERATORS`.
+By default only `next()` is available. This prevents you writing code which impacts build times.
+
+It is assumed in this section that you already monadic functional programming. This
+section is simply to explain how these are implemented in Outcome.
+
+Classic monadic programming consists of a sequence of nested functional operations:
+<dl>
+  <dt>JOIN (single): `outcome<outcome<T>>.value()` -> `outcome<T>`</dt>
+  <dt>JOIN (maximum): `outcome<outcome<outcome<outcome<T>>>>.unwrap()` -> `outcome<T>`</dt>
+    <dd>Whatever is the first monad containing a non-monad is returned.</dd>
+  <dt>MAP: `outcome<T>.map(R(T))` -> `outcome<R>`</dt>
+    <dd>If callable maps `T` to `R`, `map()` maps an `outcome<T>` to an `outcome<R>` if `outcome<T>`
+    contains a `T`. If it contains an error or is empty, that is passed through.</dd>
+  <dt>BIND: `outcome<T>.bind(outcome<R>(T))` -> `outcome<R>`</dt>
+  <dt>BIND: `outcome<T>.bind(R(T))` -> `outcome<R>`</dt>
+    <dd>If callable maps `T` to `outcome<R>` and if `outcome<T>` contains a `T`, then `bind()` maps
+    an `outcome<T>` to an `outcome<R>` else if callable maps `T` to `R` and if `outcome<T>` contains a `T`,
+    `bind()` maps an `outcome<T>` to an `outcome<R>`. In other words, returning a monad from the
+    callable does not wrap it in another monad. If the originating monad did not
+    contain a `T`, that is passed through.</dd>
+</dl>
+We also support `outcome<T>.next(R(outcome<T>))` for semantic equivalence to futures where the
+callable is called with the originating monad. This
+acts like `bind()`, so if the callable returns a monad it is not wrapped in another
+monad. Unlike `map()` or `bind()`, `next()` always calls the callable no matter what the
+monad contains, so it is up to you to interrogate the monad. Note that the originating
+monad is passed by const lvalue ref unless the callable takes a rvalue ref to the monad.
+
+A quick use example:
+\snippet monad_example.cpp monad_bind_example
+\snippet unittests.cpp monad_match_example
+\snippet unittests.cpp monad_operators_example
+
+You will note in the code example that the type of the callable for `bind()` and `map()`
+determines what operation happens. Here are the rules:
+- If the monad contains a `T` and the callable takes a `T` or an `auto`, then:
+  - If the callable takes a `T` or any reference to a `T` which isn't an rvalue reference,
+  the `T` is passed by const lvalue reference (i.e. copy semantics).
+  - If the callable takes a `T` by non-const rvalue reference, the `T` is passed by rvalue ref.
+  This lets you move from the value held by the originating monad if so desired.
+  - If the callable takes the originating monad or any reference to such which isn't a
+  rvalue reference, then the originating monad is passed by const lvalue reference.
+  - If the callable takes the originating monad by non-const rvalue reference, the
+  originating monad is passed by rvalue reference.
+The ability to take the originating monad makes `bind()` identical to `next()` though much
+harder on build times. Note that these options let you rebind the type of the monad,
+so if your callable returns a different type from the originating monad then the resulting
+monad is based on that different return type.
+\warning The current implementation requires you to specify a non-dependent return
+type for all generic lambdas, else you'll get compile errors where the compiler tried
+to insert `error_type`, `exception_type` etc when it was trying to figure out if the
+return type is correct. A future implementation (once MSVC has Expression SFINAE) may
+remove this restriction, until then just hard specify your return types if your lambdas
+take an `auto`, or use lambdas not taking `auto`.
+
+- If the monad contains an `error_type` and the callable takes an `error_type`, then
+call the callable, else pass through the monad. For this reason, any callable taking
+an `error_type` must always return the same monad type as the originating monad.
+- If the monad contains an `error_type` or an `exception_type` and the callable takes
+an `exception_type`, then call the callable, else pass through the monad. For this reason,
+any callable taking an `exception_type` must always return the same monad type as the
+originating monad.
+- If the monad is empty and the callable takes an `empty_type`, then call the callable,
+else pass through the monad. For this reason, any callable with an `empty_type` parameter must
+always return the same monad type as the originating monad.
+
+Note that for nested monads e.g. `outcome<outcome<int>>`, either or both of the inner or outer
+monads can be with value or with error or empty. You should have your binds and maps
+work appropriately.
+
+For maximum build performance, try to avoid `bind()` and `map()` as these use some hefty
+metaprogramming to deduce what kind of bind and map you're doing based on the callables
+passed. `unwrap()` is implemented using a recursively expanded structure which is probably
+okay for low unwrap depths. `next()` is probably the least weighty of the monadic operators
+as it's relatively dumb and the only metaprogramming is to determine whether to wrap
+the return type with a monad or not. Because of this impact on compile times, by default
+all operations apart from `next()` are not compiled in without the `BOOST_OUTCOME_ENABLE_OPERATORS`
+macro being defined beforehand.
+
+### Acknowledgements ###
+To T.C. on Stack Overflow for answering my question at https://stackoverflow.com/questions/30802404/how-to-detect-whether-some-callable-takes-a-rvalue-reference
+and without whose excellent answer the intelligent `map()` and `bind()` above could not work.
+
