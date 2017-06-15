3c768053be2fa09655b512c516384d39206a2fdb
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2017-06-15 19:32:21 +0100

    Latest Outcome v2 thoughts

--- include/outcome/result.hpp
+++ include/outcome/result.hpp
@@ -6,13 +6,6 @@
 #include <type_traits>
 #include <utility>  // for in_place_type_t
 
-#define BOOST_OUTCOME_THROW_SYSTEM_ERROR(ec) throw std::system_error(ec)
-#if defined(__GNUC__) || defined(__clang__)
-#define BOOST_OUTCOME_UNREACHABLE __builtin_unreachable()
-#else
-#define BOOST_OUTCOME_UNREACHABLE
-#endif
-
 namespace outcome
 {
 #if __cplusplus >= 201700
@@ -35,12 +28,87 @@ namespace outcome
     };
   }
 
+  namespace policy
+  {
+#ifdef __cpp_exceptions
+    /* \struct error_code_throw_as_system_error
+    \brief Policy interpreting EC as a type implementing the `std::error_code` contract
+    and any wide attempt to access the successful state throws the `error_code` wrapped into
+    a `std::system_error`
+    */
+    template <class EC> struct error_code_throw_as_system_error
+    {
+      static_assert(std::is_convertible<std::error_code, EC>::value, "error_type must be convertible into a std::error_code to be used with this ECPolicy");
+      //! Returns true if errored
+      template <class T, class _EC> static constexpr bool is_errored(T && /*unused*/, _EC &&_error) noexcept { return !_error; }
+      //! Performs a narrow check of state, used in the assume_value() functions
+      template <class T, class _EC> static constexpr void narrow_value_check(T && /*unused*/, _EC &&_error) noexcept
+      {
+#if defined(__GNUC__) || defined(__clang__)
+        if(_error)
+          __builtin_unreachable();
+#endif
+      }
+      //! Performs a narrow check of state, used in the assume_error() functions
+      template <class T, class _EC> static constexpr void narrow_error_check(T && /*unused*/, _EC && /*unused*/) noexcept {}
+      //! Performs a wide check of state, used in the value() functions
+      template <class T, class _EC> static constexpr void wide_value_check(T && /*unused*/, _EC &&_error)
+      {
+        if(_error)
+        {
+          throw std::system_error(_error);
+        }
+      }
+      //! Performs a wide check of state, used in the error() functions
+      template <class T, class _EC> static constexpr void wide_error_check(T && /*unused*/, _EC && /*unused*/) noexcept {}
+    };
+#endif
+    /* \struct error_code_with_terminate
+    \brief Policy interpreting EC as a type implementing the `std::error_code` contract
+    and any wide attempt to access the successful state calls `std::terminate`
+    */
+    template <class EC> struct error_code_with_terminate
+    {
+      static_assert(std::is_convertible<std::error_code, EC>::value, "error_type must be convertible into a std::error_code to be used with this ECPolicy");
+      //! Returns true if errored
+      template <class T, class _EC> static constexpr bool is_errored(T && /*unused*/, _EC &&_error) noexcept { return !_error; }
+      //! Performs a narrow check of state, used in the assume_value() functions
+      template <class T, class _EC> static constexpr void narrow_value_check(T && /*unused*/, _EC &&_error) noexcept
+      {
+#if defined(__GNUC__) || defined(__clang__)
+        if(_error)
+          __builtin_unreachable();
+#endif
+      }
+      //! Performs a narrow check of state, used in the assume_error() functions
+      template <class T, class _EC> static constexpr void narrow_error_check(T && /*unused*/, _EC && /*unused*/) noexcept {}
+      //! Performs a wide check of state, used in the value() functions
+      template <class T, class _EC> static constexpr void wide_value_check(T && /*unused*/, _EC &&_error)
+      {
+        if(_error)
+        {
+          std::terminate();
+        }
+      }
+      //! Performs a wide check of state, used in the error() functions
+      template <class T, class _EC> static constexpr void wide_error_check(T && /*unused*/, _EC && /*unused*/) noexcept {}
+    };
+  }
+
   /* \class result
   \brief Provides the result of a success and/or a failure
   \tparam T The type of the successful result. Needs to be DefaultConstructible.
   \tparam EC The type of the failure result. Needs to be DefaultConstructible and be boolean testable e.g. `if(ec)`
+  \tparam ECPolicy Policy on how to interpret type EC.
   */
-  template <class T, class EC = error_code_extended> class result
+  template <class T, class EC = error_code_extended, class ECPolicy =
+#ifdef __cpp_exceptions
+                                                     policy::error_code_throw_as_system_error<EC>
+#else
+                                                     policy::error_code_with_terminate<EC>
+#endif
+            >
+  class result
   {
     static_assert(std::is_default_constructible<T>::value, "value_type must be default constructible");
     static_assert(std::is_default_constructible<EC>::value, "error_type must be default constructible");
@@ -112,81 +180,97 @@ namespace outcome
     }
 
     //! True if has value
-    constexpr explicit operator bool() const noexcept { return !_error; }
+    constexpr explicit operator bool() const noexcept { return !ECPolicy::is_errored(_value, _error); }
     //! True if has value
-    constexpr bool has_value() const noexcept { return !_error; }
+    constexpr bool has_value() const noexcept { return !ECPolicy::is_errored(_value, _error); }
     //! True if has error
-    constexpr bool has_error() const noexcept { return _error; }
+    constexpr bool has_error() const noexcept { return ECPolicy::is_errored(_value, _error); }
 
     //! Access value directly
     value_type &assume_value() & noexcept
     {
-      if(_error)
-        BOOST_OUTCOME_UNREACHABLE;
+      ECPolicy::narrow_value_check(_value, _error);
       return _value;
     }
     const value_type &assume_value() const &noexcept
     {
-      if(_error)
-        BOOST_OUTCOME_UNREACHABLE;
+      ECPolicy::narrow_value_check(_value, _error);
       return _value;
     }
     value_type &&assume_value() && noexcept
     {
-      if(_error)
-        BOOST_OUTCOME_UNREACHABLE;
+      ECPolicy::narrow_value_check(_value, _error);
       return _value;
     }
     const value_type &&assume_value() const &&noexcept
     {
-      if(_error)
-        BOOST_OUTCOME_UNREACHABLE;
+      ECPolicy::narrow_value_check(_value, _error);
       return _value;
     }
     //! Access error directly
-    error_type &assume_error() & noexcept { return _error; }
-    const error_type &assume_error() const &noexcept { return _error; }
-    error_type &&assume_error() && noexcept { return _error; }
-    const error_type &&assume_error() const &&noexcept { return _error; }
+    error_type &assume_error() & noexcept
+    {
+      ECPolicy::narrow_error_check(_value, _error);
+      return _error;
+    }
+    const error_type &assume_error() const &noexcept
+    {
+      ECPolicy::narrow_error_check(_value, _error);
+      return _error;
+    }
+    error_type &&assume_error() && noexcept
+    {
+      ECPolicy::narrow_error_check(_value, _error);
+      return _error;
+    }
+    const error_type &&assume_error() const &&noexcept
+    {
+      ECPolicy::narrow_error_check(_value, _error);
+      return _error;
+    }
 
     //! Access value if no error
     value_type &value() &
     {
-      if(_error)
-      {
-        BOOST_OUTCOME_THROW_SYSTEM_ERROR(_error);
-      }
+      ECPolicy::wide_value_check(_value, _error);
       return _value;
     }
     const value_type &value() const &
     {
-      if(_error)
-      {
-        BOOST_OUTCOME_THROW_SYSTEM_ERROR(_error);
-      }
+      ECPolicy::wide_value_check(_value, _error);
       return _value;
     }
     value_type &&value() &&
     {
-      if(_error)
-      {
-        BOOST_OUTCOME_THROW_SYSTEM_ERROR(_error);
-      }
+      ECPolicy::wide_value_check(_value, _error);
       return _value;
     }
     const value_type &&value() const &&
     {
-      if(_error)
-      {
-        BOOST_OUTCOME_THROW_SYSTEM_ERROR(_error);
-      }
+      ECPolicy::wide_value_check(_value, _error);
       return _value;
     }
     //! Access error
-    error_type &error() & noexcept { return _error; }
-    const error_type &error() const &noexcept { return _error; }
-    error_type &&error() && noexcept { return _error; }
-    const error_type &&error() const &&noexcept { return _error; }
+    error_type &error() & noexcept
+    {
+      ECPolicy::wide_error_check(_value, _error);
+      return _error;
+    }
+    const error_type &error() const &noexcept
+    {
+      ECPolicy::wide_error_check(_value, _error);
+      return _error;
+    }
+    error_type &&error() && noexcept
+    {
+      ECPolicy::wide_error_check(_value, _error);
+      return _error;
+    }
+    const error_type &&error() const &&noexcept
+    {
+      ECPolicy::wide_error_check(_value, _error);
+      return _error;
+    }
   };
 }
 #endif
