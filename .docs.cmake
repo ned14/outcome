51fdedfb96ccf88ee904f33721e34a9b025d73fb
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2017-06-27 23:07:11 +0100

    travis

--- .docs.cmake
+++ .docs.cmake
@@ -8,6 +8,7 @@ CONFIGURE_CTEST_SCRIPT_FOR_CDASH("outcome" "cmake_ci")
 ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
 include(FindGit)
 set(CTEST_GIT_COMMAND "${GIT_EXECUTABLE}")
+find_program(HUGO_EXECUTABLE "hugo")
 #checked_execute_process("git reset"
 #  COMMAND "${GIT_EXECUTABLE}" checkout gh-pages
 #  COMMAND "${GIT_EXECUTABLE}" reset --hard ae7119571a3c81cb9a683a21f2759df1d742e998
@@ -27,7 +28,7 @@ checked_execute_process("git reset"
 #  COMMAND "${GIT_EXECUTABLE}" push -f origin gh-pages
 #  WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/doc/html"
 #)
-checked_execute_process(COMMAND "hugo"
+checked_execute_process(COMMAND "${HUGO_EXECUTABLE}"
   WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/doc/src"
 )
 ctest_submit()
