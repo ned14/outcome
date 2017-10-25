722932f504ffd14b2500a1e87ecf174631b6bf7c
Théo DELRIEU <delrieutheo@gmail.com>
2017-10-25 11:53:01 +0200

    update recipe, only retrieve master single-header

--- conanfile.py
+++ conanfile.py
@@ -1,31 +1,19 @@
-from conans import ConanFile, CMake
-import os
-
+from conans import ConanFile, tools
 
 class OutcomeConan(ConanFile):
     name = "outcome"
     version = "master"
     license = "Apache-2.0"
     description = "Provides very lightweight outcome<T> and result<T>"
-    url = "https://github.com/ned14/outcome"
-    settings = "os", "compiler", "arch", "build_type"
-    generators = "cmake"
-
-    @property
-    def outcome_src(self):
-        return os.path.join(self.source_folder, self.name)
+    repo_url = "https://github.com/ned14/outcome"
+    build_policy = "always"
 
     def source(self):
-        self.run("git clone %s --branch %s" % (self.url, self.version))
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure(source_dir=self.outcome_src, build_dir="build")
-        cmake.build()
-        cmake.test()
+        file_url = "https://raw.githubusercontent.com/ned14/outcome/master/single-header/outcome.hpp"
+        tools.download(file_url, filename="outcome.hpp")
 
     def package(self):
-        self.copy("outcome.hpp", dst="include", src=os.path.join("outcome", "single-header"))
+        self.copy("outcome.hpp", dst="include")
 
     def package_id(self):
         self.info.header_only()
