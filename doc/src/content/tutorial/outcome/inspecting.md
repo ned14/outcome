f878097c2cfa31d73ea867615d2b2db949e2fc90
Andrzej Krzemienski <akrzemi1@gmail.com>
2017-11-23 21:58:09 +0100

    docs: simpler introduction to outcome<>

--- doc/src/content/tutorial/outcome/inspecting.md
+++ doc/src/content/tutorial/outcome/inspecting.md
@@ -4,16 +4,17 @@ description = ""
 weight = 30
 +++
 
-Continuing with the previous example, in `Layer3` we obtain an `outcome<double>` which may store a `double` or an `std::error_code` or an `std::exception_ptr`.
-`Layer3` uses exceptions to indicate failure, so we want to turn a failed result into an exception: if we are storing an `std::exception_ptr` just rethrow it.
+Continuing with the previous example, in `Layer3` we have function `z` which again reports failures via exceptions.
+It will call function `h`h from `Layer2_old` which returns `outcome<int>` (which may store a `double` or an `std::error_code` or an `std::exception_ptr`).
+The goal is to unpack it to either the successful return value `int` or to throw an appropriate exception: if we are storing an `std::exception_ptr` just rethrow it.
 If we are storing an `std::error_code` throw it as `std::system_error`, which is designed to store `std::error_code`s:
 
-{{% snippet "using_outcome.cpp" "fun_3run" %}}
+{{% snippet "using_outcome.cpp" "def_z" %}}
 
 Function `has_exception` checks if it is `EP` (`std::exception_ptr`) that is stored, function `exception` accesses it. Similarly, function `error` accesses the `EC` (`std::error_code`) if it is stored.
-`outcome<>` has also function `has_failure` to check if either `EC` or `EP` is being stored. In fact, this pattern of "unpacking" `outcome<>`
-is so common that it has been implemented inside function `value`, so function `Layer3::run` could be written as:
+`outcome<>` has also function `has_failure` to check if either `EC` or `EP` is being stored. In fact, the above pattern of "unpacking" `outcome<>`
+is so common that it has been implemented inside function `value`, so function `z` could be written as:
 
 ```c++
-return Layer2NX::fun().value();
+return old::h().value();
 ```   
--- doc/src/snippets/using_outcome.cpp
+++ doc/src/snippets/using_outcome.cpp
@@ -3,123 +3,58 @@
 
 namespace outcome = OUTCOME_V2_NAMESPACE;
 
-//! [fun_1f]
-namespace Layer1
-{
-  auto f() -> int; // may throw
-}  
-//! [fun_1f]
+//! [decl_f_g]
+auto f() -> int;  // may throw
+auto g() noexcept -> outcome::result<int>;
+//! [decl_f_g]
 
-namespace Layer1
-{    
-  auto f() -> int { throw std::runtime_error{"XXX"}; }
+auto f() -> int
+{
+  throw std::runtime_error{"failure"};
 }
 
-//! [fun_2g]
-namespace Layer2NX
+auto g() noexcept -> outcome::result<int>
 {
-  auto g() noexcept -> outcome::result<int> ;
-}
-//! [fun_2g]
-
-namespace Layer2NX
-{
-  auto g() noexcept -> outcome::result<int> 
-  {
-    return 1;
-  }
+  return 1;
 }
 
-//! [fun_2f]
-namespace Layer2NX
-{   
-  auto f() noexcept -> outcome::outcome<int> 
+namespace old
+{
+    auto h() noexcept -> outcome::outcome<int>;
+}
+
+//! [def_h]
+auto old::h() noexcept -> outcome::outcome<int>
+{
+  OUTCOME_TRY(i, g());               // #1
+    
   try {
-    return Layer1::f();
+    int j = f();
+    return i + j;
   }
   catch (...) {
-    return std::current_exception();
+    return std::current_exception(); // #2
   }
-}    
-//! [fun_2f]
+}
+//! [def_h]
 
-struct Resource
+//! [def_z]
+auto z() -> int // may throw
 {
-  double use(int i) { return i * 0.01; } 
-};
-
-//! [fun_2fun]    
-namespace Layer2NX
-{  
-  auto fun() noexcept -> outcome::outcome<double> 
-  {
-    Resource* resource = new (std::nothrow) Resource;
-    if (!resource)
-        return std::errc::not_enough_memory;
-      
-    outcome::outcome<int> fr = Layer2NX::f();
-    if (!fr) {
-      delete resource;
-      return fr.as_failure();                // #1
-    }
-    
-    double ans = resource->use(fr.value());
-    
-    outcome::result<int> gr = Layer2NX::g();
-    if (!gr) {
-      delete resource;
-      return gr.as_failure();                // #2
-    }
-    
-    ans += resource->use(gr.value());
-    
-    delete resource;
-    return ans;
-  }
+  if (outcome::outcome<int> o = old::h())
+    return o.value();
+  else if (o.has_exception())
+    std::rethrow_exception(o.exception());
+  else
+    throw std::system_error{o.error()};
 }
-//! [fun_2fun]
- 
-//! [fun_2fun2]    
-namespace Layer2NX
-{  
-  auto fun2() noexcept -> outcome::outcome<double> 
-  {
-    std::unique_ptr<Resource> resource {new (std::nothrow) Resource};
-    if (!resource)
-      return std::errc::not_enough_memory;
-    
-    OUTCOME_TRY (fr, Layer2NX::f());
-    double ans = resource->use(fr);
-    
-    OUTCOME_TRY (gr, Layer2NX::g());
-    ans += resource->use(gr);
-    
-    return ans;
-  }
-}
-//! [fun_2fun2]
-
-//! [fun_3run]
-namespace Layer3
-{
-  auto run() -> double // may throw
-  {
-    if (outcome::outcome<double> o = Layer2NX::fun())
-      return o.value();
-    else if (o.has_exception())
-      std::rethrow_exception(o.exception());
-    else
-      throw std::system_error{o.error()};
-  }    
-}
-//! [fun_3run]
+//! [def_z]
 
 int main()
 try {
-  return Layer3::run();
+  return z();
 }
-catch(std::exception const& e)
-{
+catch(std::runtime_error const& e) {
   std::cerr << e.what() << std::endl;
-  return -1;
+  return 1;
 }
