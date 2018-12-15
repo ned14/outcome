e17e56d945b1e68fe00bccafe3da1c2880d9486d
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2018-12-15 13:12:51 +0000

    upd

--- include/outcome/revision.hpp
+++ include/outcome/revision.hpp
@@ -1,4 +1,4 @@
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define OUTCOME_PREVIOUS_COMMIT_REF 4995acdc40c0a888dabcaff80cef30d0a77ed9ea
-#define OUTCOME_PREVIOUS_COMMIT_DATE "2018-11-19 09:06:59 +00:00"
-#define OUTCOME_PREVIOUS_COMMIT_UNIQUE 4995acdc
+#define OUTCOME_PREVIOUS_COMMIT_REF 04bcbf4388daa31a7f7edc9debce9f2eb58446b3
+#define OUTCOME_PREVIOUS_COMMIT_DATE "2018-12-14 15:47:27 +00:00"
+#define OUTCOME_PREVIOUS_COMMIT_UNIQUE 04bcbf43
