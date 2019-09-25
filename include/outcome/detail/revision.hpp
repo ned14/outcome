d50803be3e01d38bbeb91e9c6a0ae0fb86b7c688
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2019-09-25 18:12:33 +0100

    upd

--- include/outcome/detail/revision.hpp
+++ include/outcome/detail/revision.hpp
@@ -22,6 +22,6 @@ Distributed under the Boost Software License, Version 1.0.
 */
 
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define OUTCOME_PREVIOUS_COMMIT_REF a6733b289bb613fc450ae9be7a8ec911ba07b2c2
-#define OUTCOME_PREVIOUS_COMMIT_DATE "2019-09-24 17:27:56 +00:00"
-#define OUTCOME_PREVIOUS_COMMIT_UNIQUE a6733b28
+#define OUTCOME_PREVIOUS_COMMIT_REF 90769c82ee9c1c11eef83d700108dfe3685a1cda
+#define OUTCOME_PREVIOUS_COMMIT_DATE "2019-09-25 15:08:34 +00:00"
+#define OUTCOME_PREVIOUS_COMMIT_UNIQUE 90769c82
