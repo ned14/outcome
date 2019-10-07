4fff003571497bac6b12eb0260208da5652eb3d2
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2019-10-07 15:34:01 +0100

    Implement coroutine support for Experimental Outcome.

--- include/outcome/detail/revision.hpp
+++ include/outcome/detail/revision.hpp
@@ -22,6 +22,6 @@ Distributed under the Boost Software License, Version 1.0.
 */
 
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define OUTCOME_PREVIOUS_COMMIT_REF 60894bad4f94f18ae01db924cce3b76acac35e05
-#define OUTCOME_PREVIOUS_COMMIT_DATE "2019-10-04 19:49:35 +00:00"
-#define OUTCOME_PREVIOUS_COMMIT_UNIQUE 60894bad
+#define OUTCOME_PREVIOUS_COMMIT_REF 12e86259385f1a4478252f66a2243ff993b93bc7
+#define OUTCOME_PREVIOUS_COMMIT_DATE "2019-10-07 09:46:27 +00:00"
+#define OUTCOME_PREVIOUS_COMMIT_UNIQUE 12e86259
