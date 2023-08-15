1407d02d60c604e642ae202dbbd721e8c9851fbb
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2023-08-15 20:19:21 +0100

    Also disable precompiled headers for the smoke tests, which hopefully will remediate issue #288.

--- include/outcome/detail/revision.hpp
+++ include/outcome/detail/revision.hpp
@@ -22,6 +22,6 @@ Distributed under the Boost Software License, Version 1.0.
 */
 
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define OUTCOME_PREVIOUS_COMMIT_REF 0c5b66518c843fabba9925e8750e80a5724f0918
-#define OUTCOME_PREVIOUS_COMMIT_DATE "2023-08-15 14:26:16 +00:00"
-#define OUTCOME_PREVIOUS_COMMIT_UNIQUE 0c5b6651
+#define OUTCOME_PREVIOUS_COMMIT_REF 6441e40c737a4957cfcc03d0702bf687304bd2b1
+#define OUTCOME_PREVIOUS_COMMIT_DATE "2023-08-15 19:10:51 +00:00"
+#define OUTCOME_PREVIOUS_COMMIT_UNIQUE 6441e40c
