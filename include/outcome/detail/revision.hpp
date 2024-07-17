b29317e02baaadf42f2e8466764ac7caa574ee3a
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2024-07-17 17:54:05 +0100

    Fixes to previous commits as found the code didn't link under optimisation.

--- include/outcome/detail/revision.hpp
+++ include/outcome/detail/revision.hpp
@@ -22,6 +22,6 @@ Distributed under the Boost Software License, Version 1.0.
 */
 
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define OUTCOME_PREVIOUS_COMMIT_REF 10eb45ca7d11a35df14e0fa89939a3e3185c0aa9
-#define OUTCOME_PREVIOUS_COMMIT_DATE "2024-07-16 20:39:50 +00:00"
-#define OUTCOME_PREVIOUS_COMMIT_UNIQUE 10eb45ca
+#define OUTCOME_PREVIOUS_COMMIT_REF 6279affbf2fee17dbe2a564165b8815409565b10
+#define OUTCOME_PREVIOUS_COMMIT_DATE "2024-07-17 14:25:30 +00:00"
+#define OUTCOME_PREVIOUS_COMMIT_UNIQUE 6279affb
