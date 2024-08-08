691cd9bc7f9dcf1fa17333f189751ddbdd942843
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2024-08-08 22:43:30 +0100

    Fix issue #301 where embedded GDB pretty printer was missing an escape.
    Many thanks to @k3DW for the python script to automate this.

--- include/outcome/outcome_gdb.py
+++ include/outcome/outcome_gdb.py
@@ -1,3 +1,26 @@
+# Inline GDB pretty printer for result
+# (C) 2024 Niall Douglas <http://www.nedproductions.biz/> (6 commits)
+# File Created: Jun 2024
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License in the accompanying file
+# Licence.txt or at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# Distributed under the Boost Software License, Version 1.0.
+#     (See accompanying file Licence.txt or copy at
+#           http://www.boost.org/LICENSE_1_0.txt)
+
 import gdb.printing
 import os
 
