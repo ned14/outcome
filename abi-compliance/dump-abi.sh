0022f572eb45907c7dd7f588d79fbd9f9e388f3a
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2025-05-01 20:47:38 +0100

    Fix up GA scripts to work on Ubuntu 22.04.

--- abi-compliance/dump-abi.sh
+++ abi-compliance/dump-abi.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -e
-mkdir -p build-gcc7-cxx14
-cd build-gcc7-cxx14
-CXX=g++-7 cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_STANDARD=14 -DLABEL=gcc7-cxx14
+mkdir -p build-gcc11-cxx14
+cd build-gcc11-cxx14
+CXX=g++-11 cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_STANDARD=14 -DLABEL=gcc11-cxx14
 cmake --build .
 cd ..
-mkdir -p build-gcc9-cxx17
-cd build-gcc9-cxx17
-CXX=g++-9 cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_STANDARD=17 -DLABEL=gcc9-cxx17
+mkdir -p build-gcc11-cxx17
+cd build-gcc11-cxx17
+CXX=g++-11 cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_STANDARD=17 -DLABEL=gcc11-cxx17
 cmake --build .
 cd ..
 
@@ -16,11 +16,11 @@ else
   DUMPFILE=$1
 fi
 RETCODE=0
-abi-dumper -o abi_dumps/Outcome/$DUMPFILE-gcc7-cxx14/binary_only.dump -vnum $DUMPFILE-gcc7-cxx14 build-gcc7-cxx14/liboutcome-abi-lib-gcc7-cxx14.so || RETCODE=1
-abi-dumper -o abi_dumps/Outcome/$DUMPFILE-gcc9-cxx17/binary_only.dump -vnum $DUMPFILE-gcc9-cxx17 build-gcc9-cxx17/liboutcome-abi-lib-gcc9-cxx17.so || RETCODE=1
+abi-dumper -o abi_dumps/Outcome/$DUMPFILE-gcc11-cxx14/binary_only.dump -vnum $DUMPFILE-gcc11-cxx14 build-gcc11-cxx14/liboutcome-abi-lib-gcc11-cxx14.so || RETCODE=1
+abi-dumper -o abi_dumps/Outcome/$DUMPFILE-gcc11-cxx17/binary_only.dump -vnum $DUMPFILE-gcc11-cxx17 build-gcc11-cxx17/liboutcome-abi-lib-gcc11-cxx17.so || RETCODE=1
 if [ $RETCODE -eq 0 ]; then
   cd abi_dumps/Outcome
-  tar jcf $DUMPFILE.tar.bz2 $DUMPFILE-gcc7-cxx14 $DUMPFILE-gcc9-cxx17 || true
+  tar jcf $DUMPFILE.tar.bz2 $DUMPFILE-gcc11-cxx14 $DUMPFILE-gcc11-cxx17 || true
   cd ../..
 fi
 exit $RETCODE
